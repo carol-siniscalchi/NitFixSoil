56b28f271279f0098152e98390feecc97a227319
Ryan A. Folk <folk.41@buckeyemail.osu.edu>
2023-03-07 11:02:43 -0600

    Update paths

--- scripts/DRvsNodulation.R
+++ scripts/DRvsNodulation.R
@@ -5,9 +5,11 @@ library(stringr)
 library(ggplot2)
 library(ggpubr)
 
-setwd("~/Dropbox/postdoc_MSU/linear.models.sep21/")
+
+setwd("~/Desktop/NitFixSoil-main/") # Change to top-level directory of GitHub repository
 getwd()
-DR <- read.csv("tip_DR_sep21.csv", header = TRUE, stringsAsFactors = FALSE, row.names = NULL)
+
+DR <- read.csv("./results/tip_DR_sep21.csv", header = TRUE, stringsAsFactors = FALSE, row.names = NULL)
 
 
 colnames(DR) <- c("species", "DR")
@@ -19,7 +21,7 @@ DR$genus <- rownames(DR)
 
 DR$genus <- str_replace(DR$genus, "_.*", "")
 
-nodulation <- read.csv("new.nod.status.sep21.csv")
+nodulation <- read.csv("./misc/new.nod.status.sep21.csv")
 change_list <- setNames(as.character(nodulation$Genus_Nodulation_Status), as.character(nodulation$Genus))
 DR$nodulation <- lapply(DR$genus, function(x) if(any(!is.na(x))) change_list[x] else NA)
 
@@ -73,25 +75,26 @@ subfamily<-ggplot(DR.fixed, aes(x = subfamily, y = log(DR), fill = subfamily)) +
 subanalysis<-ggplot(DR.fixed, aes(x = subanalysis, y = log(DR), fill = subanalysis)) + geom_violin(trim = TRUE) + 
   ylim(min(DR.fixed$DR), log(100)) + scale_fill_brewer(palette="RdYlBu") + theme(legend.position="none")
 
-pdf(file="DR.composite.pdf", width = 24, height = 10)
+pdf(file="./results/DR.composite.pdf", width = 24, height = 10)
 ggarrange(DR.type, DR.nod, subanalysis, 
           labels = c("A", "B", "C"),
           ncol = 3, nrow = 1)
 dev.off()
 
-pdf(file="comp2.pdf", width = 20, height = 7)
+pdf(file="./results/comp2.pdf", width = 20, height = 7)
 ggarrange(nodstatus, nodtype, subfamily, 
           #labels = c("A", "B", "C"),
           ncol = 3, nrow = 1)
 dev.off()
 
-tree = read.tree("nitfix.finalgenbank_Feb2021_onlygenusspecies_renamed.noduplicates.tre")
-svl<-read.csv("tip_DR_sep21.csv",header=TRUE,row.names=1)
+tree = read.tree("./phylogenetic_trees/nitfix.finalgenbank_Feb2021_onlygenusspecies_renamed.noduplicates.tre")
+
+svl<-read.csv("./results/tip_DR_sep21.csv",header=TRUE,row.names=1)
 log.svl <-log1p(svl)
 log.svl<-setNames(log.svl[,1],rownames(log.svl))
 obj<-contMap(tree,log.svl,plot=FALSE)
 obj<-setMap(obj,invert=TRUE)
-pdf(file="reconstruction.pdf", width = 500, height = 500)
+pdf(file="./results/reconstruction.pdf", width = 500, height = 500)
 plot(obj,fsize=c(0.1,0.1),outline=FALSE,lwd=c(1,3),leg.txt="log(SVL)")
 dev.off()
 
